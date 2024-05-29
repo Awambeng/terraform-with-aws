ef39b1a32bc787c0d967ff6908b28eeb9a697465
Awambeng <awambengrodrick@gmail.com>
2024-05-29 12:28:25 +0100

    update terraform config

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -29,8 +29,8 @@ terraform {
 }
 
 provider "aws" {
-    access_key = "access_key"
-    secret_key = "secret_key"
+    # access_key = "access_key"
+    # secret_key = "secret_key"
     region = "us-east-1"
 }
 
